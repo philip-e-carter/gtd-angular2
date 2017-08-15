9a202e2113f8193c0ce5055d064bd07b582c730a
philip e carter <philip.e.carter@gmail.com>
2017-08-15 15:01:04 -0500

    refactored for GTD.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Task } from './task';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'GTD';
 }
